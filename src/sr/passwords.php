ac955c5fe32796ea82d3d90ec88d221230295def
Milan Markovic <milan@viewpnt.com>
2019-07-03 13:54:44 +0200

    changes to ro, sr, bg, added de-AT

--- src/sr/passwords.php
+++ src/sr/passwords.php
@@ -16,5 +16,5 @@ return [
     'reset'    => 'Lozinka je resetovana!',
     'sent'     => 'Poslali smo reset link za vašu lozinku!',
     'token'    => 'Ukucana oznaka za resetovanje lozinke nije važeća.',
-    'user'     => 'Nismo uspeli pronaći korisnika sa email adresom.',
+    'user'     => 'Nismo uspeli pronaći korisnika sa ovom email adresom.',
 ];
